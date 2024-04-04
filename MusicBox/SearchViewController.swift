219e30dbb0cc8eb5088a945087a7b7174a45c3a6
Artem Tkachev <tkachevforios@icloud.com>
2024-04-04 21:49:28 +0300

    SearchVC: setup table view, searchBar. Create TkackModel

--- MusicBox/SearchViewController.swift
+++ MusicBox/SearchViewController.swift
@@ -9,9 +9,44 @@ import UIKit
 
 class SearchViewController: UITableViewController {
     
+    var tracksArray = [TrackModel(trackName: "1", artistName: "A"), TrackModel(trackName: "2", artistName: "B"), TrackModel(trackName: "3", artistName: "C")]
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemOrange
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CellID")
+        setupSearchBar()
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+//        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+}
+
+extension SearchViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracksArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellID", for: indexPath)
+        let track = tracksArray[indexPath.row]
+        cell.textLabel?.numberOfLines = 2
+        cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+        cell.imageView?.image = UIImage(systemName: "music.note")
+        return cell
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
     }
 }
